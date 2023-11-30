9ac520306b42fbec727008c00083d3a9c04ba482
Saya Deprez <saya.deprez@gmail.com>
2023-11-30 20:22:05 +0100

    add style-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {FooterComponent} from "./footer/footer.component";
+import {StylePageComponent} from "./style-page/style-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,RouterLink, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet,RouterLink, NavbarComponent, FooterComponent,StylePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from "./home-page/home-page.component";
+import {StylePageComponent} from "./style-page/style-page.component";
 
 export const routes: Routes = [
-  {path:"", component:HomePageComponent}
+  {path:"", component:HomePageComponent},
+//   TODO: hide test page unless developer after login
+  {path:"style", component:StylePageComponent},
 ];
