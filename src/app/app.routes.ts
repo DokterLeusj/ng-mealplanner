7e79834e2ea5b9519f5b1669dac7268e17ea1899
Kevin Uyttebroeck <uyttebroeckkevin@gmail.com>
2023-12-04 12:47:14 +0100

    41-recipe-detail-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from "./home/home-page/home-page.component";
 import {StylePageComponent} from "./style-page/style-page.component";
+import {RecipeDetailComponent} from "./recipe/recipe-detail/recipe-detail.component";
 
 export const routes: Routes = [
   {path:"", component:HomePageComponent},
 //   TODO: hide test page unless developer after login
   {path:"style", component:StylePageComponent},
-  {path:"recipe/:id", component:}
+  {path:"recipe/1", component:RecipeDetailComponent}
 ];
