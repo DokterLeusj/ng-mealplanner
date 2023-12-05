020546fd9c9fe6cd86ba3c5b5b9c6373031329f2
Saya Deprez <saya.deprez@gmail.com>
2023-12-05 09:07:13 +0100

    add mock recipe to recipes-page ts, adjust layout of recipe card

--- src/app/recipe/model/recipe-list-dto.ts
+++ src/app/recipe/model/recipe-list-dto.ts
@@ -9,3 +9,4 @@ export interface RecipeListDto {
     username:string;
   }
 }
+
--- src/app/recipe/recipes-page/recipes-page.component.ts
+++ src/app/recipe/recipes-page/recipes-page.component.ts
@@ -1,24 +1,149 @@
 import {Component} from '@angular/core';
 import {RecipeCardComponent} from "../recipe-card/recipe-card.component";
 import {RecipeListDto} from "../model/recipe-list-dto";
-import {RecipeService} from "../../recipe.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-recipes-page',
   standalone: true,
   imports: [
     RecipeCardComponent,
+    NgForOf,
   ],
   templateUrl: './recipes-page.component.html',
   styleUrl: './recipes-page.component.css'
 })
 export class RecipesPageComponent {
-  recipes:Array<RecipeListDto>=[];
+  recipes: Array<RecipeListDto> = [
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem, commodi corporis dolor, eaque eligendi error esse est maxime nesciunt officiis porro recusandae, repudiandae saepe sint ut vel vitae voluptatem." ,
+      imgUrl: null,
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem, commodi corporis." ,
+      imgUrl:  'https://placehold.co/100x200.png?text=Sorry!+No+image+yet&font=lato',
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl:  'https://placehold.co/200x100.png?text=Sorry!+No+image+yet&font=lato',
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl:  'https://placehold.co/300x300.png?text=Sorry!+No+image+yet&font=lato',
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl:  'https://placehold.co/1000x2000.png?text=Sorry!+No+image+yet&font=lato',
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl: null,
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl: null,
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl: null,
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl: null,
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl: null,
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
+    {
+      id: 1,
+      name: "Garlic Pasta",
+      description: null,
+      imgUrl: null,
+      nutriTech: false,
+      "author": {
+        "id": 1,
+        "username": "hello"
+      }
+    },
 
-  constructor(private recipeService:RecipeService) {
-    this.recipeService.getAllRecipes().subscribe(response =>{
-      this.recipes=response;
-    })
-  }
+
+  ];
+
+  // constructor(private recipeService:RecipeService) {
+  //   this.recipeService.getAllRecipes().subscribe(response =>{
+  //     this.recipes=response;
+  //   })
+  // }
 
 }
