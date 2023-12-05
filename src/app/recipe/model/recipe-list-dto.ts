18ba95b86dc9336de22d0ab8b5267b461116af5a
Saya Deprez <saya.deprez@gmail.com>
2023-12-05 10:56:56 +0100

    add provider so recipe service works

--- src/app/recipe/model/recipe-list-dto.ts
+++ src/app/recipe/model/recipe-list-dto.ts
@@ -8,5 +8,7 @@ export interface RecipeListDto {
     id:number;
     username:string;
   }
+
+
 }
 
--- src/app/recipe/recipes-page/recipes-page.component.ts
+++ src/app/recipe/recipes-page/recipes-page.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {RecipeCardComponent} from "../recipe-card/recipe-card.component";
 import {RecipeListDto} from "../model/recipe-list-dto";
 import {NgForOf} from "@angular/common";
+import {RecipeService} from "../../recipe.service";
 
 @Component({
   selector: 'app-recipes-page',
@@ -14,136 +15,138 @@ import {NgForOf} from "@angular/common";
   styleUrl: './recipes-page.component.css'
 })
 export class RecipesPageComponent {
-  recipes: Array<RecipeListDto> = [
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem, commodi corporis dolor, eaque eligendi error esse est maxime nesciunt officiis porro recusandae, repudiandae saepe sint ut vel vitae voluptatem." ,
-      imgUrl: null,
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem, commodi corporis." ,
-      imgUrl:  'https://placehold.co/100x200.png?text=Sorry!+No+image+yet&font=lato',
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl:  'https://placehold.co/200x100.png?text=Sorry!+No+image+yet&font=lato',
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl:  'https://placehold.co/300x300.png?text=Sorry!+No+image+yet&font=lato',
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl:  'https://placehold.co/1000x2000.png?text=Sorry!+No+image+yet&font=lato',
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl: null,
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl: null,
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl: null,
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl: null,
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl: null,
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
-    {
-      id: 1,
-      name: "Garlic Pasta",
-      description: null,
-      imgUrl: null,
-      nutriTech: false,
-      "author": {
-        "id": 1,
-        "username": "hello"
-      }
-    },
+  recipes: Array<RecipeListDto> =[];
+  constructor(private recipeService:RecipeService) {
+    this.recipeService.getAllRecipes().subscribe(response =>{
+      this.recipes=response;
+    })
+  }
+  // recipes: Array<RecipeListDto> = [
+  //   {
+  //     id: 1,
+  //     name: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem, commodi corporis",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem, commodi corporis dolor, eaque eligendi error esse est maxime nesciunt officiis porro recusandae, repudiandae saepe sint ut vel vitae voluptatem." ,
+  //     imgUrl: null,
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem, commodi corporis." ,
+  //     imgUrl:  'https://placehold.co/100x200.png?text=Sorry!+No+image+yet&font=lato',
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl:  'https://placehold.co/200x100.png?text=Sorry!+No+image+yet&font=lato',
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl:  'https://placehold.co/300x300.png?text=Sorry!+No+image+yet&font=lato',
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl:  'https://placehold.co/1000x2000.png?text=Sorry!+No+image+yet&font=lato',
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl: null,
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl: null,
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl: null,
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl: null,
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl: null,
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Garlic Pasta",
+  //     description: null,
+  //     imgUrl: null,
+  //     nutriTech: false,
+  //     "author": {
+  //       "id": 1,
+  //       "username": "hello"
+  //     }
+  //   },
+  //
+  //
+  // ];
 
 
-  ];
-
-  // constructor(private recipeService:RecipeService) {
-  //   this.recipeService.getAllRecipes().subscribe(response =>{
-  //     this.recipes=response;
-  //   })
-  // }
 
 }
