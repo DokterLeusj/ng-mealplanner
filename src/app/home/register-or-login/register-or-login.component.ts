b5925eedf8a3ac03deaf0acbbf01ac01e42e2371
Saya Deprez <saya.deprez@gmail.com>
2023-12-07 23:11:11 +0100

    add validations and error messages

--- src/app/home/register-or-login/register-or-login.component.ts
+++ src/app/home/register-or-login/register-or-login.component.ts
@@ -1,15 +1,75 @@
 import {Component} from '@angular/core';
-import {RegisterOrLoginFormComponent} from "../register-or-login-form/register-or-login-form.component";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-register-or-login',
   standalone: true,
   imports: [
-    RegisterOrLoginFormComponent
+    ReactiveFormsModule,
+    NgClass,
+    NgIf
   ],
   templateUrl: './register-or-login.component.html',
   styleUrl: './register-or-login.component.css'
 })
 export class RegisterOrLoginComponent {
+  isRegisterNotLogin: boolean=true;
+  changeToggleRegisterNotLogin(): void {
+    // this.isRegisterNotLogin = !this.isRegisterNotLogin; // Login not implemented yet
+  }
+  showingPassword:boolean=false;
+  isButtonActionClicked:boolean=false;
+
+  userRegisterEmail = new FormControl("");
+  userRegisterPassword = new FormControl("");
+
+  getPasswordType(): string {
+    return this.showingPassword ? 'text' : 'password';
+  }
+  toggleShowingPassword(): void {
+    this.showingPassword = !this.showingPassword;
+  }
+
+  runActionButtonMethods():void{
+    this.isButtonActionClicked=true;
+    this.isRegisterNotLogin?this.attemptRegister():this.attemptLogin();
+  }
+  attemptRegister() {
+  // if email already registered, display error message
+    // if email not registered yet, display registration successful & go to Home
+  }
+
+  attemptLogin() {
+  }
+
+  // Validation
+  showErrorFormatValidation():boolean{
+    return this.isButtonActionClicked&&!this.isValidFormatCredentials();
+  }
+  showErrorCredentialsValidation():boolean {
+    return this.isButtonActionClicked&&this.isValidFormatCredentials()&&!this.isValidCredentials();
+  }
+  getDisabledMessage():string {
+    return "Please make all fields valid.";
+  }
+  getInvalidCredentialsMessage():string{
+    return "This email address is already in use."
+  }
+
+  isValidEmail():boolean{
+    return this.userRegisterEmail.value!="";
+  }
+  isValidPassword():boolean{
+    return this.userRegisterPassword.value!="";
+  }
+
+  isValidFormatCredentials():boolean {
+    return (this.isValidEmail()&&this.isValidPassword());
+  }
+
+  isValidCredentials():boolean{
+    return false;
+  }
 
 }
