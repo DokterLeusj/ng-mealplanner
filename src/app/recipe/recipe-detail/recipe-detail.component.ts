c348a678e4b7e39453327bc43be2c8457e7ff209
Kevin Uyttebroeck <uyttebroeckkevin@gmail.com>
2023-12-07 21:30:42 +0100

    add ordering on the incoming response. instructions & ingredients both respectively sorted

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -4,6 +4,8 @@ import {ActivatedRoute} from "@angular/router";
 import {NgForOf, NgOptimizedImage} from "@angular/common";
 import {RecipeDetailDto} from "../model/recipe-detail-dto";
 import {RecipeListDto} from "../model/recipe-list-dto";
+import {RecipeInstructionDto} from "../model/recipe-instruction-dto";
+import {RecipeIngredientDto} from "../model/recipe-ingredient-dto";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -21,6 +23,9 @@ export class RecipeDetailComponent {
   constructor(private recipeService: RecipeService, private route: ActivatedRoute) {
     this.route.params.subscribe(params => {
       this.recipeService.getRecipeById(params['id']).subscribe((response:RecipeDetailDto )=>{
+        response.instructions.sort((a:RecipeInstructionDto, b:RecipeInstructionDto) => a.step - b.step)
+        response.ingredients.sort((a:RecipeIngredientDto, b:RecipeIngredientDto) =>
+        a.ingredient.name.localeCompare(b.ingredient.name))
         this.recipe = response;
       })
     })
