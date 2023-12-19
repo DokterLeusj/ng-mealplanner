ae44db3c5d38b7fe02d52a393fd988b55f5e4507
Saya Deprez <saya.deprez@gmail.com>
2023-12-19 16:43:41 +0100

    create components for filter and day card of mealplan

---
+++ src/app/mealplan/plan-day-card/plan-day-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-day-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './plan-day-card.component.html',
+  styleUrl: './plan-day-card.component.css'
+})
+export class PlanDayCardComponent {
+
+}
---
+++ src/app/mealplan/plan-filter/plan-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './plan-filter.component.html',
+  styleUrl: './plan-filter.component.css'
+})
+export class PlanFilterComponent {
+
+}
