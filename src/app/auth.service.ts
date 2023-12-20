84b3bbd5645c51adcaba1cf8fa5bf7f5466fa182
Kevin Uyttebroeck <uyttebroeckkevin@gmail.com>
2023-12-20 12:44:43 +0100

    add attemptRegister

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,93 +1,109 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {catchError, map, Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {catchError, map, Observable, of, switchMap, throwError} from "rxjs";
 import {AuthUser} from "./user/model/auth-user";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthService {
-    private readonly BASE_API_URL: string = 'http://localhost:8080/api/v1';
-    private readonly AUTH_URL: string = this.BASE_API_URL + '/auth';
-    private loggedInAuthUser: AuthUser | null = null;
+  private readonly BASE_API_URL: string = 'http://localhost:8080/api/v1';
+  private readonly AUTH_URL: string = this.BASE_API_URL + '/auth';
+  private loggedInAuthUser: AuthUser | null = null;
 
-    constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getLoggedInAuthUser(): AuthUser {
+    if (this.loggedInAuthUser == null) {
+      try {
+        this.loggedInAuthUser = this.getLoggedInAuthUserFromLocalStorage();
+      } catch (e) {
+        throw e;
+      }
     }
+    return this.loggedInAuthUser;
+  }
 
-    public getLoggedInAuthUser(): AuthUser {
-        if (this.loggedInAuthUser == null) {
-            try {
-                this.loggedInAuthUser = this.getLoggedInAuthUserFromLocalStorage();
-            } catch (e) {
-                throw e;
-            }
-        }
-        return this.loggedInAuthUser;
+  public logOut(): void {
+    this.loggedInAuthUser = null;
+    localStorage.removeItem('loggedInAuthUser');
+  }
+
+  public attemptLogin(email: string, password: string): Observable<any> {
+    return this.sendLoginRequest(email, password)
+      .pipe(
+        map(response => {
+          this.setLoggedInAuthUserInLocalStorage(response.id, email, password);
+          return response;
+        })
+        ,
+        catchError(e => {
+          console.error("Invalid credentials", e);
+          throw e;
+        })
+      )
+  }
+
+  // public attemptLogin(email: string, password: string) {
+  //     this.sendLoginRequest(email, password)
+  //         .pipe(
+  //             catchError(e => {
+  //                 console.error("Invalid credentials", e);
+  //                 throw e;
+  //             })
+  //         )
+  //         .subscribe(
+  //             response => {
+  //                 this.setLoggedInAuthUserInLocalStorage(response.id, email, password);
+  //             });
+  //
+  // }
+
+  public attemptRegister(email: string, password: string): Observable<any> {
+    return this.sendRegisterRequest(email, password)
+      .pipe(
+        map(() => {
+          const successMessage: string = 'Successfully registered. you can now log in as ' + email
+          return of(successMessage).pipe(
+            switchMap(() => this.attemptLogin(email, password)))
+        }),
+        catchError((error) => {
+          console.error('Registration failed', error);
+          return throwError(() => new Error('Registration failed'));
+        })
+      )
+  };
+
+
+  private getAuthToken(email: string, password: string): string {
+    return window.btoa(email + ':' + password);
+  }
+
+  private setLoggedInAuthUserInLocalStorage(id: string, email: string, password: string) {
+    const authToken: string = this.getAuthToken(email, password)
+    this.loggedInAuthUser = {
+      "id": id,
+      "basicAuthToken": authToken
     }
+    localStorage.setItem('loggedInAuthUser', JSON.stringify(this.loggedInAuthUser));
+  }
 
-    public logOut(): void {
-        this.loggedInAuthUser = null;
-        localStorage.removeItem('loggedInAuthUser');
+  private getLoggedInAuthUserFromLocalStorage(): AuthUser {
+    const storedUser = localStorage.getItem('loggedInAuthUser');
+    if (!storedUser) {
+      throw new Error('No logged in user stored');
     }
+    return JSON.parse(storedUser);
+  }
 
-    public attemptLogin(email: string, password: string):Observable<any> {
-       return this.sendLoginRequest(email, password)
-            .pipe(
-                map(response => {
-                    this.setLoggedInAuthUserInLocalStorage(response.id, email, password);
-                    return response;
-                })
-                ,
-                catchError(e => {
-                    console.error("Invalid credentials", e);
-                    throw e;
-                })
-            )
-    }
-    // public attemptLogin(email: string, password: string) {
-    //     this.sendLoginRequest(email, password)
-    //         .pipe(
-    //             catchError(e => {
-    //                 console.error("Invalid credentials", e);
-    //                 throw e;
-    //             })
-    //         )
-    //         .subscribe(
-    //             response => {
-    //                 this.setLoggedInAuthUserInLocalStorage(response.id, email, password);
-    //             });
-    //
-    // }
+  private sendLoginRequest(email: string, password: string): Observable<any> {
+    return this.httpClient.post(this.AUTH_URL + '/login',
+      {"email": email, "password": password});
+  }
 
-    public attemptRegister(email: string, password: string): boolean {
-        return false;
-        // if email already registered, display error message
-        // if email not registered yet, display registration successful & go to Home
-    }
-
-    private getAuthToken(email: string, password: string): string {
-        return window.btoa(email + ':' + password);
-    }
-
-    private setLoggedInAuthUserInLocalStorage(id: string, email: string, password: string) {
-        const authToken: string = this.getAuthToken(email, password)
-        this.loggedInAuthUser = {
-            "id": id,
-            "basicAuthToken": authToken
-        }
-        localStorage.setItem('loggedInAuthUser', JSON.stringify(this.loggedInAuthUser));
-    }
-
-    private getLoggedInAuthUserFromLocalStorage(): AuthUser {
-        const storedUser = localStorage.getItem('loggedInAuthUser');
-        if (!storedUser) {
-            throw new Error('No logged in user stored');
-        }
-        return JSON.parse(storedUser);
-    }
-
-    private sendLoginRequest(email: string, password: string): Observable<any> {
-        return this.httpClient.post(this.AUTH_URL + '/login', {"email": email, "password": password});
-
-    }
+  private sendRegisterRequest(email: string, password: string): Observable<any> {
+    return this.httpClient.post<string>(this.AUTH_URL + '/register',
+      {"email": email, "password": password});
+  }
 }
